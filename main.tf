cc136e73e64efbf47847e421842c2809330a8149
soujanvangapandu <vangapandusoujan6@gmail.com>
2023-09-17 13:47:29 +0530

    parameters

--- main.tf
+++ main.tf
@@ -27,12 +27,16 @@ module "alb" {
 }
 
 module "docdb" {
-  source            = "git::https://github.com/vsoujan/tf-module-docdb.git"
-  tags              = var.tags
-  env               = var.env
-
-  for_each          = var.docdb
-  subnet_ids        = local.db_subnets
-
+  source                    = "git::https://github.com/vsoujan/tf-module-docdb.git"
+  tags                      = var.tags
+  env                       = var.env
+  for_each                  = var.docdb
+  subnet_ids                = local.db_subnets
+  backup_retention_period   = each.value["backup_retention_period"]
+  preferred_backup_window   = each.value["preferred_backup_window"]
+  skip_final_snapshot       = each.value["skip_final_snapshot"]
+  vpc_id                    = local.vpc_id
+  sg_ingress_cidr           = local.app_subnets_cidr
+  engine_version            = each.value["engine_version"]
 }
 
